bbc50e89236c7435adb439f1ad7624b6b1d1749c
Gabor Heja <gheja@w>
2016-10-25 18:08:30 +0200

    Updated formatting.

--- hook.sh
+++ hook.sh
@@ -8,18 +8,22 @@ set -o pipefail
 
 case "$1" in
 	"deploy_challenge")
-		echo "Add the following to the zone definition of ${2}:"
-		echo "  _acme-challenge.${2}. IN TXT \"${4}\""
 		echo ""
-		echo -n "When ready, press enter to continue..."
+		echo "Add the following to the zone definition of ${2}:"
+		echo "_acme-challenge.${2}. IN TXT \"${4}\""
+		echo ""
+		echo -n "Press enter to continue..."
 		read tmp
+		echo ""
 	;;
 	"clean_challenge")
-		echo "Now you can remove the following from the zone definition of ${2}:"
-		echo "  _acme-challenge.${2}. IN TXT \"${4}\""
 		echo ""
-		echo -n "When ready, press enter to continue..."
+		echo "Now you can remove the following from the zone definition of ${2}:"
+		echo "_acme-challenge.${2}. IN TXT \"${4}\""
+		echo ""
+		echo -n "Press enter to continue..."
 		read tmp
+		echo ""
 	;;
 	"deploy_cert")
 		# do nothing for now
